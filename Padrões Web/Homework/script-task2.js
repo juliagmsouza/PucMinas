93623a7cf87ea6852133f076c31f79670885d72c
juliagmsouza <juliagmsouza12@gmail.com>
2023-10-09 23:00:53 -0300

    feat:cria instagram da bolha dev - atividade passada em sala

--- Padrões Web/Homework/script-task2.js
+++
@@ -1,8 +0,0 @@
-// Selection.classList.add('')
-// Selection.style.backgroundColor = "blue"
-
-// const button = document.getElementById("my-button")
-// GamepadButton.addEventListener('click', function(){
-//     alert("Button was clicked")
-// })
-
--- Padrões Web/Instagram/app2.js
+++ Padrões Web/Instagram/app2.js
@@ -1,12 +1,25 @@
 function main() {
    const form = document.getElementById('catForms');
-   console.log('form', form)
-   form.addEventListener('submit', () => {
-      console.log('adanona')
-    const textarea = document.getElementById('comment-cats');
-    const p = document.getElementById('comments');
-    p.style.display = 'block';
-    p.textContent = textarea.value;
+   form.addEventListener('click', () => {
+      const textarea = document.getElementById('comment-cats');
+      const p = document.getElementById('comments');
+      const date = new Date();
+      p.style.display = 'block';
+      p.innerHTML = '<strong>BikeVibe: </strong>' + textarea.value + `<small class="float-right">${date.toLocaleString()}</small>`
+      textarea.value = '';
+   });
+   const btn1 = document.getElementById('followBtn1');
+   btn1.addEventListener('click', () => {
+      btn1.textContent = btn1.textContent === 'Seguir' ? 'Deixar de seguir' : 'Seguir';
+   });
+   const btn2 = document.getElementById('followBtn2');
+   btn2.addEventListener('click', () => {
+      btn2.textContent = btn2.textContent === 'Seguir' ? 'Deixar de seguir' : 'Seguir';
+   });
+   const btn3 = document.getElementById('followBtn3');
+   btn3.addEventListener('click', () => {
+      btn3.textContent = btn3.textContent === 'Seguir' ? 'Deixar de seguir' : 'Seguir';
+      btn3.classList.add('btn-primary')
    });
 };
 
