4ebb33809679a86aaecaca83f93aed26c17195aa
Júlia Maia <47127259+juliagmsouza@users.noreply.github.com>
2023-11-28 12:00:21 -0300

    Update index.js

--- PUC - PWN/index.js
+++ PUC - PWN/index.js
@@ -1,7 +1,7 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
 
 const listaProdutos = {
     produtos: [
